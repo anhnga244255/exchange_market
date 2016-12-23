987e30ab8c8cdb553bcaf98002b78fc334b2fe1c
Nguyễn Anh Nga <anhnga244255@gmail.com>
2016-12-24 02:08:57 +0700

    Add files via upload

---
+++ delete.php
@@ -0,0 +1,13 @@
+<?php 
+error_reporting(2048);
+header('Content-Type: text/html;charset=UTF-8');
+require_once("config.php");
+require_once("model/model.php");
+if ($_GET['pk']) {
+	$table = "products";
+	$where = "ID = ".$_GET['pk'];
+	delete($table, $where);
+	header("Location: /exchange_market/admin/?page=".$_GET['page']);
+} else {
+	header("Location: /exchange_market/admin");
+}
